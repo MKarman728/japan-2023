7ed08106aa21aadd870b6dc2ade41acac6d6798f
Mark Karman <mkarman08@gmail.com>
2023-09-20 16:06:03 -0700

    added password protection and entire kamikochi

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Set password with pw variable
+const pw = 'ramen';
+const passwordInput = document.getElementById('password');
+const passwordContainer = document.getElementById('password-container');
+const navbar = document.getElementById('navbar');
+
+
+function passwordVerification(e) {
+    e.preventDefault();
+    if (passwordInput.value === pw) {
+        localStorage.setItem('pw', passwordInput.value);
+        navbar.style.display = 'flex';
+        passwordContainer.style.display = 'none';
+    }
+    else {
+        passwordInput.value = '';
+    }
+}
+
+function passwordCheck() {
+    const passwordVerification = localStorage.getItem('pw');
+    if (passwordVerification === pw) {
+        navbar.style.display = 'flex';
+        passwordContainer.style.display = 'none';
+    }
+}
+
+document.getElementById('password-submit').addEventListener('click', passwordVerification);
+document.addEventListener('DOMContentLoaded', passwordCheck);
